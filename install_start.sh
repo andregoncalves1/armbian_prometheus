5ada1cf69994773822bae7a87e03b0897a328ae7
andre <andregoncalves1@gmail.com>
2021-03-20 22:25:37 +0000

    Fix prometheus version to ARM

--- install_start.sh
+++ install_start.sh
@@ -5,7 +5,7 @@ for i in rules rules.d files_sd; do sudo mkdir -p /etc/prometheus/${i}; done
 sudo apt update
 sudo apt -y install wget curl vim
 mkdir -p /tmp/prometheus && cd /tmp/prometheus
-curl -s https://api.github.com/repos/prometheus/prometheus/releases/latest | grep browser_download_url | grep linux-amd64 | cut -d '"' -f 4 | wget -qi -
+curl -s https://github.com/prometheus/prometheus/releases/download/v2.22.0/prometheus-2.22.0.linux-armv7.tar.gz | grep browser_download_url | grep linux-amd64 | cut -d '"' -f 4 | wget -qi -
 tar xvf prometheus*.tar.gz
 cd prometheus*/
 sudo mv prometheus promtool /usr/local/bin/
