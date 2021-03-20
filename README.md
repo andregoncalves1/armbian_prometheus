2a6451724d6f4dd02ef4ffc98954cc8c1c989e2c
andre <andregoncalves1@gmail.com>
2021-03-20 21:48:17 +0000

    Add first scripts

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# armbian_prometheus
+# Armbian Prometheus
+
+## Install
+
+- Run install_start.sh
+- Copy prometheus config to /etc/prometheus/ 
+
+```sh
+sudo cp <repo_clone>/prometheus.yml /etc/prometheus/
+```
+
+- Copy prometheus systemd to /etc/systemd/system/ 
+
+```sh
+sudo cp <repo_clone>/prometheus.service /etc/systemd/system/
+```
+- Run install_finish.sh
---
+++ install_finish.sh
@@ -0,0 +1,8 @@
+for i in rules rules.d files_sd; do sudo chown -R prometheus:prometheus /etc/prometheus/${i}; done
+for i in rules rules.d files_sd; do sudo chmod -R 775 /etc/prometheus/${i}; done
+sudo chown -R prometheus:prometheus /var/lib/prometheus/
+sudo systemctl daemon-reload
+sudo systemctl start prometheus
+sudo systemctl enable prometheus
+systemctl status prometheus
+
